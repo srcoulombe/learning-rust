60b741878fa5a0a7cc2bd8bbf58ffc2a7a271981
samy.coulombe <samy.coulombe@mail.mcgill.ca>
2019-10-06 18:09:12 -0400

    added some comments to hello_world.rs

--- hello_world/hello_world.rs
+++ hello_world/hello_world.rs
@@ -1,4 +1,13 @@
 // Hello World!
 fn main() {
+    // from:
+    // https://doc.rust-lang.org/rust-by-example/hello.html
     println!( "Hello world!" );
+    println!("I'm a (small) Rustacean!")
 }
+
+// This is a comment
+
+/*
+ * This is a comment block
+*/
